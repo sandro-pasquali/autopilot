c3099346e48d9c70ae91d4ef878671b92db9828d
sandro-pasquali <spasquali@gmail.com>
2015-04-04 11:16:39 -0500

    fix config.json absence in clone process

--- bin/defaults.sh
+++ bin/defaults.sh
@@ -1,7 +1,8 @@
 #	These are the defaults for the system. Some of them can be changed via
-#	`npm run-script config`. The others can be changed here...but 
-#	you probably don't need to.
-#
+#	`npm run-script config`. You shouldn't change the others. If you must,
+#	then you will need to manually restart any production servers, inserting
+#	and using the new settings, and possibly other repos.
+#	
 export SOURCE_DIR=source
 export SOURCE_VIEWS_DIR=source/views
 export SOURCE_TEMPLATES_DIR=source/templates
@@ -20,7 +21,6 @@ export SERVER_ROUTES=./router/routes
 export WORKING_FOLDER=/.swanson
 export LOG_FILE=/.swanson/output.log
 export LEVEL_DATA_DIR=./data
-export SESSION_SECRET=somesecretstring
 
 #	This last group is exposed/changeable when you run configuration tool.
 #
@@ -30,4 +30,8 @@ export HOST=127.0.0.1
 export PORT=2122
 export NUM_CLUSTER_CORES=0
 export DEV_AUTO_RELOAD=yes
+#	This is generated automatically on prod start, so the default
+#	value here has no influence on the final value.
+#
+export SESSION_SECRET=somesecretstring
 
