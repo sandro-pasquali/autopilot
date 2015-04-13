0afa709eac837863373fcebcf47ee311874a6056
Sandro Pasquali <spasquali@gmail.com>
2015-04-13 17:06:51 -0500

    add build queue

--- api/log/index.js
+++ api/log/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var fs 		= require('fs');
+var path	= require('path');
 var util 	= require('util');
 var Promise = require('bluebird');
 var bunyan 	= require('bunyan');
---
+++ swanson/buildQueue.js
@@ -0,0 +1,112 @@
+var fork = require('child_process').fork;
+var path = require('path');
+var Promise = require('bluebird');
+var env = require('../env');
+var api = require('../api');
+
+var log 	= api.log.create('build:');
+var cache	= api.cache.create('buildqueue:');
+
+var error = function(err) {
+	if(error) {
+		log.error(err);
+	}
+};
+
+var complete = function() {
+	log.info('Build completed');
+	
+	//	Remove last build from queue
+	//
+	cache.get('data').then(function(obj) {
+		if(!obj) {
+			return;
+		}
+		
+		//	Lose the head (the last build), see if there
+		//	is a queued build, and if there is, run that.
+		//
+		obj.list.shift();
+		
+		cache.set('data', obj).then(function() {
+			if(obj.list.length) {
+				add('push', obj.list[0]);
+			}
+		}).catch(log.error);		
+	}).catch(log.error);
+};
+
+var list = function() {
+	return new Promise(function(resolve, reject) {
+		cache.get('data').then(function(obj) {
+			resolve(obj.list);
+		}).catch(log.error);
+	});
+};
+
+var add = function(event, manifestJSON) {
+
+	return new Promise(function(resolve, reject) {
+		if(!event || !manifestJSON) {
+			return reject(new TypeError('#add received bad arguments'));
+		}
+		cache.get('data').then(function(obj) {
+		
+			var data = obj;
+			
+			//	#data key is never deleted once created; we only
+			//	push/shift from data.list. So this is rarely necessary.
+			//
+			if(data === null) {
+				data = {
+					started: 0,
+					list: []
+				};					
+			}
+			
+			var queueRequest = !!list.length;
+			
+			//	Either way, we're adding to the queue
+			//
+			data.list.push(manifestJSON);
+			
+			//	If there is something in the list (queue), queue
+			//	this request.
+			//
+			if(queueRequest) {
+				cache.set('data', data)
+				.then(function() {
+					resolve(true);
+				})
+				.catch(log.error);
+				
+				return;
+			}
+			
+			//	Queue is clean. Add the current and start build.
+			//
+			cache.set('data', data).then(function() {
+				//	TODO: will prob. want to store some aspects of the
+				//	manifest in api.log, and do something re: event type.
+				//	now just "push"
+				//
+				try {
+					fork(__dirname + '/push.js', [manifestJSON]);
+				} catch(e) {
+					log.error(e);
+				}
+				
+				resolve(false);
+				
+			}).catch(log.error);
+			
+		}).catch(log.error);
+	});
+};
+
+module.exports = {
+	list : list,
+	add : add,
+	error : error,
+	complete : complete
+};
