4c142f2fe552e69b29b9c3f80c70a2d8fd2b3786
Sandro Pasquali <spasquali@gmail.com>
2015-05-05 16:06:51 -0500

    added test

--- spec/test_api_email.js
+++ spec/test_api_email.js
@@ -1,9 +1,13 @@
-
 var email = api.email.create('sendGrid');
 
 email.send({
 	to:       'spasquali@gmail.com',
 	from:     'spasquali@gmail.com',
 	subject:  'Subject goes here',
-	text:     'Hello world'
+	text:     'Hello world',
+	foo: 1
+}).then(function(res) {
+	console.log(res);
+}).catch(function(err) {
+	console.log(err);
 });
